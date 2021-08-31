4fd06c5202652919ccf4c1566c06b50bbdac1094
Drumly_x <73012508+DrummerSly@users.noreply.github.com>
2021-08-31 19:39:50 +0800

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,4 @@
+using autoClicker;
 using System;
 using System.ComponentModel;
 using System.Drawing;
@@ -5,7 +6,7 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
 
-namespace autoClicker
+namespace autoClicker2
 {
     public partial class Form1 : Form
     {
@@ -15,6 +16,7 @@ namespace autoClicker
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
 
+        // Hex codes for mouse buttons
         private const int LEFTUP = 0x0004;
         private const int LEFTDOWN = 0x0002;
         private const int MIDDLEUP = 0x0040;
@@ -22,16 +24,20 @@ namespace autoClicker
         private const int RIGHTUP = 0x0010;
         private const int RIGHTDOWN = 0x0008;
 
+        // If you want to change any of these values, you can but you need to change the textbox interval's text too
         public int milisecondsIntervals = 100;
         public int secondsIntervals = 000;
         public int minutesIntervals = 0;
         public int hoursIntervals = 0;
-        public int parsedValue;
+        
+        public int parsedValue; // The value to check if the character you put in textbox interval's is numeric
 
         public bool enabled = false;
         
         public string clickType = "Single";
         public string mouseButton = "Left";
+
+        Form2 f2 = new Form2();
 
         public Form1()
         {
@@ -41,12 +47,13 @@ namespace autoClicker
         private void Form1_Load(object sender, EventArgs e)
         {
             CheckForIllegalCrossThreadCalls = false;
-            Thread AC = new Thread(AutoClick);
-            EnableACHotkey.RunWorkerAsync();
+            Thread AC = new Thread(AutoClick); // Converts the void to thread or something
+            EnableACHotkey.RunWorkerAsync(); // Starts the AC Hotkey
             ClickTypeDropDownList.Text = "Single";
             MouseButtonDropDownList.Text = "Left";
+            MinimizedNotifyIcon.Visible = false;
 
-            AC.Start();
+            AC.Start(); // Starts AutoClick()
         }
 
         private void AutoClick()
@@ -55,10 +62,26 @@ namespace autoClicker
             {
                 if (enabled == true)
                 {
-                    if (mouseButton == "Left")
+                    if (mouseButton == "Left") // Checks if MouseButtonDropDownList's text is Left
                     {
-                        if (clickType == "Single")
+                        if (clickType == "Single") // Checks if ClickTypeDropDownList's text is Single
                         {
+                            mouse_event(dwFlags: LEFTUP, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0); // Holds down mouse left button
+                            Thread.Sleep(1); // Thread.Sleep(); is basically wait() for some coding languages but it's different
+
+                            mouse_event(dwFlags: LEFTDOWN, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0); // Releases mouse left button
+                            Thread.Sleep(int.Parse(hoursIntervals.ToString().PadRight(7, '0'))); // Thread.Sleep(??00000); ("?" means the text you put in textbox interval's
+                            Thread.Sleep(int.Parse(minutesIntervals.ToString().PadRight(5, '0'))); // Thread.Sleep(?0000);
+                            Thread.Sleep(int.Parse(secondsIntervals.ToString().PadRight(4, '0'))); // Thread.Sleep(?000);
+                            Thread.Sleep(milisecondsIntervals); // Thread.SLeep(?);
+                        } else if (clickType == "Double") // Checks if ClickTypeDropDownList's text is Double
+                        {
+                            mouse_event(dwFlags: LEFTUP, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
+                            Thread.Sleep(1);
+
+                            mouse_event(dwFlags: LEFTDOWN, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
+                            Thread.Sleep(1);
+
                             mouse_event(dwFlags: LEFTUP, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
                             Thread.Sleep(1);
 
@@ -67,23 +90,7 @@ namespace autoClicker
                             Thread.Sleep(int.Parse(minutesIntervals.ToString().PadRight(5, '0')));
                             Thread.Sleep(int.Parse(secondsIntervals.ToString().PadRight(4, '0')));
                             Thread.Sleep(milisecondsIntervals);
-                        } else if (clickType == "Double")
-                        {
-                            mouse_event(dwFlags: LEFTUP, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
-                            Thread.Sleep(1);
-
-                            mouse_event(dwFlags: LEFTDOWN, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
-                            Thread.Sleep(1);
-
-                            mouse_event(dwFlags: LEFTUP, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
-                            Thread.Sleep(1);
-
-                            mouse_event(dwFlags: LEFTDOWN, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
-                            Thread.Sleep(int.Parse(hoursIntervals.ToString().PadRight(7, '0')));
-                            Thread.Sleep(int.Parse(minutesIntervals.ToString().PadRight(5, '0')));
-                            Thread.Sleep(int.Parse(secondsIntervals.ToString().PadRight(4, '0')));
-                            Thread.Sleep(milisecondsIntervals);
-                        } else if (clickType == "Triple")
+                        } else if (clickType == "Triple") // Checks if ClickTypeDropDownList's text is Triple
                         {
                             mouse_event(dwFlags: LEFTUP, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
                             Thread.Sleep(1);
@@ -109,7 +116,7 @@ namespace autoClicker
                             Thread.Sleep(milisecondsIntervals);
                         }
                     }
-                    else if (mouseButton == "Middle")
+                    else if (mouseButton == "Middle") // Checks if MouseButtonDropDownList's text is Middle
                     {
                         if (clickType == "Single")
                         {
@@ -161,7 +168,7 @@ namespace autoClicker
                             Thread.Sleep(milisecondsIntervals);
                         }
                     }
-                    else if (mouseButton == "Right")
+                    else if (mouseButton == "Right") // Checks if MouseButtonDropDownList's text is Right
                     {
                         if (clickType == "Single")
                         {
@@ -234,13 +241,13 @@ namespace autoClicker
 
         private void Miliseconds_TextChanged(object sender, EventArgs e)
         {
-            if (!int.TryParse(Miliseconds.Text, out parsedValue))
+            if (!int.TryParse(Miliseconds.Text, out parsedValue)) // Checks if the character you put in textbox interval's is numeric
             {
                 MessageBox.Show("Please enter a number");
                 return;
             } else
             {
-                milisecondsIntervals = int.Parse(Miliseconds.Text);
+                milisecondsIntervals = int.Parse(Miliseconds.Text); // Makes the interval's value to the text of textbox interval's
             }
         }
 
@@ -266,7 +273,7 @@ namespace autoClicker
             }
             else
             {
-                if (int.Parse(Minutes.Text) != 0)
+                if (int.Parse(Minutes.Text) != 0) // Checks if the numeric isn't 0
                 {
                     minutesIntervals = int.Parse(Minutes.Text) + 5; 
                 }
@@ -295,7 +302,7 @@ namespace autoClicker
             {
                 if (enabled == true)
                 {
-                    if (GetAsyncKeyState(Keys.F7)< 0)
+                    if (GetAsyncKeyState(Keys.F7)< 0) // Checks if you pressed F7
                     {
                         StartButton.Enabled = true;
                         enabled = false;
@@ -315,20 +322,25 @@ namespace autoClicker
 
         private void Form1_SizeChanged(object sender, EventArgs e)
         {
-            if (this.WindowState == FormWindowState.Minimized)
+            if (this.WindowState == FormWindowState.Minimized) // Checks if the window is minimized
             {           
-                this.ShowInTaskbar = false;
-                MinimizedNotifyIcon.Icon = this.Icon;
-                MinimizedNotifyIcon.BalloonTipTitle = "Auto Clicker has been minimized";
-                MinimizedNotifyIcon.BalloonTipText = "Click the icon to open the application again";
-                MinimizedNotifyIcon.ShowBalloonTip(1000);
+                if (f2.minimizeToTray == true)
+                {
+                    this.ShowInTaskbar = false;
+                    MinimizedNotifyIcon.Visible = true;
+                    MinimizedNotifyIcon.Icon = this.Icon;
+                    MinimizedNotifyIcon.BalloonTipTitle = "Auto Clicker has been minimized";
+                    MinimizedNotifyIcon.BalloonTipText = "Click the icon to open the application again";
+                    MinimizedNotifyIcon.ShowBalloonTip(1000); // Shows the notify icon to the taskbar for 1 second
+                }
             }
         }
 
-        private void MinimizedNotifyIcon_MouseClick(object sender, MouseEventArgs e)
+        private void MinimizedNotifyIcon_MouseClick(object sender, MouseEventArgs e) // If they pressed the notify icon
         {
             this.ShowInTaskbar = true;
             this.WindowState = FormWindowState.Normal;
+            MinimizedNotifyIcon.Visible = false;
         }
 
         private void MouseButtonDropDownList_SelectedIndexChanged(object sender, EventArgs e)
@@ -339,6 +351,11 @@ namespace autoClicker
         private void ClickTypeDropDownList_SelectedIndexChanged(object sender, EventArgs e)
         {
             clickType = ClickTypeDropDownList.Text;
+        }
+
+        private void OptionsButton_Click(object sender, EventArgs e)
+        {
+            f2.Show(); // Shows Options window
         }
     }
 }
