74adce86cd437463a84d0d70fe29c11b1386ab07
Drumly_x <73012508+DrummerSly@users.noreply.github.com>
2022-01-14 15:42:51 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 using autoClicker2;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace autoClicker
@@ -13,6 +10,7 @@ namespace autoClicker
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+
         static void Main()
         {
             Application.EnableVisualStyles();
